b9892e46a4420025964f626eb84bb943c5c376f1
laendasill <panador@gmail.com>
2015-09-15 08:48:46 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/question', function(req, res, next){
+  fs.readFile("temp\\SymboleWarszawskie.json", function(err,data){
+    if(err){
+      throw err;
+    }
+      res.json(JSON.parse(data));
+  });
+
+});
+
+module.exports = router;
