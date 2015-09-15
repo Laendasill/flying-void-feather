8eea80486d6cfe5f62fbacc50361e2485561d5a9
laendasill <panador@gmail.com>
2015-09-15 22:38:53 +0200

    trying to get rid of security policy

--- app.js
+++ app.js
@@ -6,24 +6,28 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-
+var setup  = require('./routes/setup');
 
 var app = express();
-
+var helmet = require('helmet');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(helmet.csp({
+  defaultSrc: ["'self'", 'localhost:3000'],
+  scriptSrc: ["'self'",   'localhost:3000/javascripts']
+}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'node_modules/etnografia_gra/build/script')));
 app.use('/', routes);
-
+app.use('/setup', setup);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
